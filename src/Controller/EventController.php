1685100118ade4ef452d8a66ab65e5f056607de6
JeremyDallain <jeremy.dallain@gmail.com>
2021-02-17 17:48:32 +0100

    Creation du controller 'Event' et de ses routes. Rajout de la methode asset() pour le cablage des fichiers dans le dossier public #1 Necessite 'composer require symfony/asset'.

---
+++ src/Controller/EventController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EventController extends AbstractController
+{
+    /**
+     * @Route("/event", name="event")
+     */
+    public function index()
+    {
+        return $this->render('event/index.html.twig');
+    }
+
+    /**
+     * @Route("/event/{category_slug}/{slug}", name="event_show")
+     */
+    public function show($slug)
+    {
+        return $this->render('event/show.html.twig', [
+            'slug' => $slug
+        ]);
+    }
+
+    /**
+     * @Route("/event/{category_slug}", name="event_category")
+     */
+    public function category($category_slug)
+    {
+        return $this->render('event/category.html.twig', [
+            'categorySlug' => $category_slug
+        ]);
+    }
+
+    /**
+     * @Route("/organizer/create", name="event_create")
+     */
+    public function create()
+    {
+        // formualire de creation de l'event
+
+        // lorsque le formulaire est soumis, ou est ce redirigé ? à voir ?
+
+        return $this->render('event/create.html.twig');
+    }
+
+    /**
+     * @Route("/organizer/update/{id}", name="event_update")
+     */
+    public function update($id)
+    {
+        // formulaire pour update l'event avec l'{id}
+
+        // lorsque le formulaire est soumis, ou est ce redirigé ? à voir ?
+
+        return $this->render('event/update.html.twig', [
+            'id' => $id
+        ]);
+    }
+
+    /**
+     * @Route("/organizer/delete/{id}", name="event_delete")
+     */
+    public function delete($id)
+    {
+        
+        // traitement de la suppression
+
+        // redirection
+
+        return $this->render('event/delete.html.twig', [
+            'id' => $id
+        ]);
+    }
+}
