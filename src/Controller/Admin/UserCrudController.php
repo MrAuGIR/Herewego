a86ba90e4de2d1a0bbc24be88e1dfa8e9109ae3f
MrAuGir <augirard17@gmail.com>
2021-03-07 21:39:08 +0100

    crud utilisateur partie admin

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -2,14 +2,18 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
+use App\Entity\Localisation;
+use App\Form\RegisterType;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * @isGranted("ROLE_ADMIN", statusCode=404, message="404 page not found")
@@ -17,13 +21,25 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
  */
 class UserCrudController extends AbstractController
 {
+
+    protected $encoder;
+    protected $em;
+    
+
+    public function __construct(UserPasswordEncoderInterface $encoder, EntityManagerInterface $em)
+    {
+        $this->encoder = $encoder;
+        $this->em = $em;
+    }
+
+
     /**
      * @Route("/", name="usercrud")
      */
     public function index(UserRepository $userRepository): Response
     {
 
-        $users = $userRepository->findAll();
+        $users = $userRepository->findByRole('ROLE_USER');
 
 
         return $this->render('admin/user/users.html.twig', [
@@ -32,14 +48,114 @@ class UserCrudController extends AbstractController
     }
 
     /**
-     * @Route("/create", name="usercrud_create")
+     * @Route("/show/{id}", name="usercrud_show")
      */
-    public function create(): Response
+    public function show(User $user)
     {
 
-        return $this->render('admin/user.html.twig', [
+        return $this->render('admin/user/show.html.twig', [
+            'user' => $user,
             'controller_name' => 'AdminController',
         ]);
+    }
+
+    /**
+     * @Route("/create", name="usercrud_create")
+     */
+    public function create(Request $request): Response
+    {
+        $user = new User();
+
+        $form = $this->createForm(RegisterType::class,$user, ['chosen_role' => ['ROLE_USER']]);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            /*Localisation de l'utilisateur*/
+            $localisation = new Localisation();
+            $localisation->setAdress($request->request->get('register')['localisation']['adress'])
+                ->setCityName($request->request->get('register')['localisation']['cityName'])
+                ->setCityCp($request->request->get('register')['localisation']['cityCp'])
+                ->setCoordonneesX($request->request->get('register')['localisation']['coordonneesX'])
+                ->setCoordonneesY($request->request->get('register')['localisation']['coordonneesY']);
+
+            $this->em->persist($localisation);
+
+            /* creation de l'organisateur*/
+            $hash = $this->encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($hash)
+                ->setIsValidate(true) // Comme c'est l'admin qui crée le user, il est validé dés le départ
+                ->setIsPremium(False)
+                ->setRoles(['ROLE_USER'])
+                ->setRegisterAt(new \DateTime())
+                ->setLocalisation($localisation);
+
+            $this->em->persist($user);
+
+            $this->em->flush();
+
+            $this->addFlash('success', 'Utilisateur enregistré');
+            return $this->redirectToRoute('usercrud');
+        }
+
+        return $this->render('admin/user/create.html.twig', [
+            'form'=>$form->createView(),
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+
+    /**
+     * @Route("/edit/{id}", name="usercrud_edit")
+     */
+    public function edit(User $user, Request $request):Response
+    {
+        $form = $this->createForm(RegisterType::class, $user, ['chosen_role' => ['ROLE_USER']]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /*Localisation de l'utilisateur*/
+            $localisation = $user->getLocalisation();
+            $localisation->setAdress($request->request->get('register')['localisation']['adress'])
+                ->setCityName($request->request->get('register')['localisation']['cityName'])
+                ->setCityCp($request->request->get('register')['localisation']['cityCp'])
+                ->setCoordonneesX($request->request->get('register')['localisation']['coordonneesX'])
+                ->setCoordonneesY($request->request->get('register')['localisation']['coordonneesY']);
+
+            $this->em->persist($localisation);
+
+            /* creation de l'organisateur*/
+            $hash = $this->encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($hash)
+                ->setIsPremium(False)
+                ->setRoles(['ROLE_USER'])
+                ->setLocalisation($localisation);
+
+            $this->em->persist($user);
+
+            $this->em->flush();
+
+            $this->addFlash('success', 'Utilisateur modifié');
+            return $this->redirectToRoute('usercrud');
+        }
+
+        return $this->render('admin/user/edit.html.twig', [
+            'user'=> $user,
+            'form'=> $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="usercrud_delete")
+     */
+    public function delete(User $user, Request $request)
+    {
+        $this->em->remove($user);
+        $this->em->flush();
+
+        $this->addFlash('success',"Utilisateur supprimé");
+        $this->redirectToRoute('usercrud');
     }
 
 }
