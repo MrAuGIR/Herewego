9a8ebc698c5ee7785ba486c0cc1f1a73aed167bb
MrAuGir <augirard17@gmail.com>
2021-03-07 17:04:55 +0100

    creation du controller admin et suppression du bundle easyAdmin

---
+++ src/Controller/Admin/AdminController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+/**
+ * @isGranted("ROLE_ADMIN", statusCode=404, message="404 page not found")
+ */
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+}
