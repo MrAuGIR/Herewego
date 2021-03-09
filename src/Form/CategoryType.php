cb2310d7857cc30431042a6ada4c48672442d95b
MrAuGir <augirard17@gmail.com>
2021-03-09 22:43:27 +0100

    crud category (manque le edit)

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -8,7 +8,10 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ColorType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+<<<<<<< HEAD
 use Symfony\Component\Validator\Constraints\NotBlank;
+=======
+>>>>>>> 7c864d5 (crud category (manque le edit))
 use Symfony\Component\Form\FormBuilderInterface;
 
 class CategoryType extends AbstractType
@@ -16,6 +19,7 @@ class CategoryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('name',TextType::class,[
+<<<<<<< HEAD
             'label' => 'Nom',
             'attr' => ['placeholder'=>'nom de la category'],
             'constraints' => [
@@ -23,14 +27,22 @@ class CategoryType extends AbstractType
                     'message' => 'Le nom de la catégorie est obligatoire',
                 ]),
             ]
+=======
+            'label'=>'Nom',
+            'attr'=>['placeholder'=>'nom de la category']
+>>>>>>> 7c864d5 (crud category (manque le edit))
         ])
         ->add('color',ColorType::class,[
             'label'=>'Couleur',
         ])
         ->add('pathLogo',FileType::class,[
+<<<<<<< HEAD
             'label' => 'Logo',
             'mapped'=> false,
             'required' => false,
+=======
+            'label'=>'Logo',
+>>>>>>> 7c864d5 (crud category (manque le edit))
         ])
         ;
     }
