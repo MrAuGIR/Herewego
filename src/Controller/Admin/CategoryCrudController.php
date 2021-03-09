f2fff9d5d1dd165e358c27d6e95e1290ebc44633
MrAuGir <augirard17@gmail.com>
2021-03-09 22:43:27 +0100

    crud category (manque le edit)

--- src/Controller/Admin/CategoryCrudController.php
+++ src/Controller/Admin/CategoryCrudController.php
@@ -21,7 +21,10 @@ use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Flex\Path;
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 7c864d5 (crud category (manque le edit))
 /**
  * @isGranted("ROLE_ADMIN", statusCode=404, message="404 page not found")
  * @Route("/admin/category")
@@ -81,6 +84,7 @@ class CategoryCrudController extends AbstractController
             /*gestion du logo */
             $logo = $form->get('pathLogo')->getData();
 
+<<<<<<< HEAD
             if($logo != null){
                 //on genere un nouveau nom de fichier (codé) et on rajoute son extension
                 $fichier = md5(uniqid()) . '.' . $logo->guessExtension();
@@ -91,6 +95,20 @@ class CategoryCrudController extends AbstractController
                 // on stock l'image dans la bdd (son nom)
                 $category->setPathLogo($fichier);
             }
+=======
+            //on genere un nouveau nom de fichier (codé) et on rajoute son extension
+            $fichier = md5(uniqid()) . '.' . $logo->guessExtension();
+
+            // on copie le fichier dans le dossier uploads
+            // 2 params (destination, fichier)
+            $logo->move(
+                $this->getParameter('logo_directory'),
+                $fichier
+            );
+            // on stock l'image dans la bdd (son nom)
+            $category->setPathLogo($fichier);
+
+>>>>>>> 7c864d5 (crud category (manque le edit))
 
             $category->setName($request->request->get('category')['name'])
                 ->setSlug(strtolower($this->slugger->slug($category->getName())))
@@ -98,9 +116,17 @@ class CategoryCrudController extends AbstractController
 
 
             $this->em->persist($category);
+<<<<<<< HEAD
             $this->em->flush();
 
             $this->addFlash('success', 'categorie ajouté');
+=======
+
+
+            $this->em->flush();
+
+            $this->addFlash('success', 'categorie modifié');
+>>>>>>> 7c864d5 (crud category (manque le edit))
             return $this->redirectToRoute('categorycrud');
         }
 
@@ -121,13 +147,21 @@ class CategoryCrudController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+<<<<<<< HEAD
             $logo = $form->get('pathLogo')->getData();
 
+=======
+            
+            /*gestion du logo */
+            $logo = $form->get('logo')->getData();
+            
+>>>>>>> 7c864d5 (crud category (manque le edit))
             //on genere un nouveau nom de fichier (codé) et on rajoute son extension
             $fichier = md5(uniqid()) . '.' . $logo->guessExtension();
 
             // on copie le fichier dans le dossier uploads
             // 2 params (destination, fichier)
+<<<<<<< HEAD
             $logo->move( $this->getParameter('logo_directory'),$fichier );
 
             /* Penser a supprimer les ancien fichiers  */
@@ -143,6 +177,24 @@ class CategoryCrudController extends AbstractController
                 ->setColor($request->request->get('category')['color']);
             
             $this->em->persist($category);
+=======
+            $logo->move(
+                $this->getParameter('logo_directory'),
+                $fichier
+            );
+            // on stock l'image dans la bdd (son nom)
+            $category->setPathLogo($fichier);
+            
+            
+            $category->setName($request->request->get('category')['name'])
+                ->setSlug(strtolower($this->slugger->slug($category->getName())))
+                ->setColor($request->request->get('category')['color']);
+                
+
+            $this->em->persist($category);
+
+        
+>>>>>>> 7c864d5 (crud category (manque le edit))
             $this->em->flush();
 
             $this->addFlash('success', 'categorie modifié');
