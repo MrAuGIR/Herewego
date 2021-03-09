7c864d515b7ec3b9f0b9998ffb125ad8ce873c8f
MrAuGir <augirard17@gmail.com>
2021-03-09 22:43:27 +0100

    crud category (manque le edit)

---
+++ src/Form/CategoryType.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Form;
+
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ColorType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class CategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name',TextType::class,[
+            'label'=>'Nom',
+            'attr'=>['placeholder'=>'nom de la category']
+        ])
+        ->add('color',ColorType::class,[
+            'label'=>'Couleur',
+        ])
+        ->add('pathLogo',FileType::class,[
+            'label'=>'Logo',
+        ])
+        ;
+    }
+}
