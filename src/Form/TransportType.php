ef55eb718c30b78a6e1460b1e983c324966e352d
MrAuGir <augirard17@gmail.com>
2021-03-14 13:36:26 +0100

    constraints entity localidation et transport pour les formulaires de transport

--- src/Form/TransportType.php
+++ src/Form/TransportType.php
@@ -14,6 +14,10 @@ use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\GreaterThan;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotNull;
 
 
 
@@ -30,15 +34,20 @@ class TransportType extends AbstractType
                 'label'=> 'Quand ?',
                 'widget' => 'single_text',
                 'html5'=> true,
-                'attr'=> ['placeholder'=> 'Date de départ',
-                            'min'=> $now.'T00:00'] //$now->format('Y-m-dTH:i')]
+                'attr'=> [
+                    'placeholder'=> 'Date de départ',
+                    'min'=> $now.'T00:00'
+                ],
+                
             ])
             ->add('goEndedAt',DateTimeType::class, [
                 'label'=> 'Arrivé ? (heure approximative)',
                 'widget' => 'single_text',
                 'html5' => true,
-                'attr'=>['placeholder'=> 'Date et heure d\'arrivé',
-                         'min'=> $now.'T00:00']
+                'attr'=> [
+                    'placeholder'=> 'Date et heure d\'arrivé',
+                    'min'=> $now.'T00:00'
+                ],
             ])
             //Localisation de retour
             ->add('localisation_return', LocalisationType::class)
@@ -46,27 +55,38 @@ class TransportType extends AbstractType
                 'label' => 'Quand ?',
                 'widget' => 'single_text',
                 'html5' => true,
-                'attr' => ['placeholder' => 'Date de retour',
-                            'min' => $now .'T00:00']
+                'attr' => [
+                    'placeholder' => 'Date de retour',
+                    'min' => $now .'T00:00'
+                ],
             ])
             ->add('returnEndedAt', DateTimeType::class, [
                 'label' => 'Arrivé ? (heure approximative)',
                 'widget' => 'single_text',
                 'html5' => true,
-                'attr' => ['placeholder' => 'Date et heure d\'arrivé au retour',
-                            'min' => $now . 'T00:00']
+                'attr' => [
+                    'placeholder' => 'Date et heure d\'arrivé au retour',
+                    'min' => $now . 'T00:00'
+                ],
             ])
             ->add('placePrice', MoneyType::class, [
                 'label' => 'Prix par place',
+                'required' => false,
                 'attr' => ['placeholder'=> 'Prix/place']
             ])
             ->add('totalPlace', IntegerType::class, [
                 'label' => 'Nombre de place',
+                'required' => false,
                 'attr' => ['placeholder'=> 'nombre de place']
             ])
             ->add('commentary', TextAreaType::class, [
                 'label' => 'Informations complémentaires',
+                'required' => false,
                 'attr'=>['placeholder'=> 'Indiquer des informations supplémentaires sur le transport']
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-primary',
+                            'value' => 'Créé']
             ])
     
             
