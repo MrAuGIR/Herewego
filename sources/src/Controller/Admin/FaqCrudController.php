5cbfa7ea3527574a473709a928200225bbe6c321
MrAuGIR <augirard17@gmail.com>
2025-03-07 23:26:29 +0100

    wip: route controller Admin/FaqCrudController

--- sources/src/Controller/Admin/FaqCrudController.php
+++ sources/src/Controller/Admin/FaqCrudController.php
@@ -8,36 +8,29 @@ use App\Form\QuestionAdminType;
 use App\Repository\QuestionAdminRepository;
 use App\Repository\QuestionUserRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
-/**
- * @isGranted("ROLE_ADMIN", statusCode=404, message="404 page not found")
- *
- * @Route("/admin/faq")
- */
+#[Route('/admin/faq')]
+#[\Symfony\Component\Security\Http\Attribute\IsGranted("ROLE_ADMIN", message: "404 page not found", statusCode: 404)]
 class FaqCrudController extends AbstractController
 {
-    protected $em;
-    protected $slugger;
-    protected $qUserRepo;
-    protected $qAdminRepo;
-
-    public function __construct(EntityManagerInterface $em, SluggerInterface $slugger, QuestionUserRepository $qUserRepo, QuestionAdminRepository $qAdminRepo)
+    public function __construct(
+        protected EntityManagerInterface $em,
+        protected SluggerInterface $slugger,
+        protected QuestionUserRepository $qUserRepo,
+        protected QuestionAdminRepository $qAdminRepo
+    )
     {
-        $this->em = $em;
-        $this->slugger = $slugger;
-        $this->qUserRepo = $qUserRepo;
-        $this->qAdminRepo = $qAdminRepo;
     }
 
-    /**
-     * @Route("/", name="faqcrud")
-     */
-    public function index()
+
+    #[Route("/", name: "faqcrud")]
+    public function index(): Response
     {
         /* On recupère les questions des utilisateurs */
         $questionsUser = $this->qUserRepo->findAll();
@@ -47,17 +40,14 @@ class FaqCrudController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/show/qUser/{id}", name="faqcrud_qUser_show")
-     */
+    #[Route("/show/qUser/{id}", name: "faqcrud_qUser_show", methods: ["GET"])]
     public function show(QuestionUser $questionUser)
     {
     }
 
-    /**
-     * @Route("/delete/qUser/{id}", name="faqcrud_qUser_delete")
-     */
-    public function delete(QuestionUser $questionUser)
+
+    #[Route("/delete/qUser/{id}", name: "faqcrud_qUser_delete", methods: [Request::METHOD_DELETE])]
+    public function delete(QuestionUser $questionUser): RedirectResponse
     {
         if (! $questionUser) {
             $this->addFlash('danger', "la question demandé n'existe pas");
@@ -73,10 +63,9 @@ class FaqCrudController extends AbstractController
         return $this->redirectToRoute('faqcrud');
     }
 
-    /**
-     * @Route("/liste", name="faqcrud_qAdmin_liste")
-     */
-    public function liste()
+
+    #[Route("/liste", name: "faqcrud_qAdmin_liste", methods: [Request::METHOD_GET])]
+    public function liste(): Response
     {
         /* On recupère les questions rédigé par les administrateurs */
         $questionsAdmin = $this->qAdminRepo->findAll();
@@ -84,10 +73,8 @@ class FaqCrudController extends AbstractController
         return $this->render('admin/faq/liste.html.twig', compact('questionsAdmin'));
     }
 
-    /**
-     * @Route("/create/qAdmin", name="faqcrud_qAdmin_create")
-     */
-    public function create(Request $request)
+    #[Route("/create/qAdmin", name: "faqcrud_qAdmin_create", methods: [Request::METHOD_POST])]
+    public function create(Request $request): RedirectResponse|Response
     {
         $questionAdmin = new QuestionAdmin();
 
@@ -111,10 +98,8 @@ class FaqCrudController extends AbstractController
         return $this->render('admin/faq/create.html.twig', ['form' => $form->createView()]);
     }
 
-    /**
-     * @Route("/edit/qAdmin/{id}", name="faqcrud_qAdmin_edit")
-     */
-    public function edit(QuestionAdmin $questionAdmin, Request $request)
+    #[Route("/edit/qAdmin/{id}", name: "faqcrud_qAdmin_edit", methods: [Request::METHOD_PUT])]
+    public function edit(QuestionAdmin $questionAdmin, Request $request): RedirectResponse|Response
     {
         $form = $this->createForm(QuestionAdminType::class, $questionAdmin);
 
@@ -136,10 +121,8 @@ class FaqCrudController extends AbstractController
         return $this->render('admin/faq/edit.html.twig', ['form' => $form->createView()]);
     }
 
-    /**
-     * @Route("/delete/qAdmin/{id}", name="faqcrud_qAdmin_delete")
-     */
-    public function deleteQAdmin(QuestionAdmin $questionAdmin)
+    #[Route("/delete/qAdmin/{id}", name: "faqcrud_qAdmin_delete", methods: [Request::METHOD_DELETE])]
+    public function deleteQAdmin(QuestionAdmin $questionAdmin): RedirectResponse
     {
         if (! $questionAdmin) {
             $this->addFlash('danger', 'Question inexistante');
@@ -155,10 +138,8 @@ class FaqCrudController extends AbstractController
         return $this->redirectToRoute('faqcrud_qAdmin_liste');
     }
 
-    /**
-     * @Route("/show/qAdmin/{id}", name="faqcrud_qAdmin_show")
-     */
-    public function showQAdmin(QuestionAdmin $questionAdmin)
+    #[Route("/show/qAdmin/{id}", name: "faqcrud_qAdmin_show", methods: [Request::METHOD_GET])]
+    public function showQAdmin(QuestionAdmin $questionAdmin): RedirectResponse
     {
         if (! $questionAdmin) {
             $this->addFlash('danger', 'Question inexistante');
