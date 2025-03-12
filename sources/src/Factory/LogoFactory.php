68fea26de93115e44d0625fa4b062a2a63f1f410
MrAuGIR <augirard17@gmail.com>
2025-03-13 00:22:29 +0100

    wip: AdminCategoryController

---
+++ sources/src/Factory/LogoFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Category;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Form\FormInterface;
+
+class LogoFactory
+{
+    public function __construct(
+        #[Autowire(param: 'logo_directory')]
+        private string $logoDirectory
+    ) {
+    }
+
+    public function handleFromForm(FormInterface $form, Category $category): ?string
+    {
+        $logo = $form->get('pathLogo')->getData();
+
+        if (empty($logo)) {
+            return null;
+        }
+
+        $file = md5(uniqid()) . '.' . $logo->guessExtension();
+
+        $logo->move(
+            $this->logoDirectory,
+            $file
+        );
+
+        return $file;
+
+    }
+}
