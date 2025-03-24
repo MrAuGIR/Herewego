c09260e8afcab59cb1f94b16237a28d9e48807b9
MrAuGIR <augirard17@gmail.com>
2025-03-24 23:08:43 +0100

    EventVoter participate event and cancel participate event

--- sources/src/Security/Voter/EventVoter.php
+++ sources/src/Security/Voter/EventVoter.php
@@ -3,7 +3,9 @@
 namespace App\Security\Voter;
 
 use App\Entity\Event;
+use App\Entity\Participation;
 use App\Entity\User;
+use App\Repository\ParticipationRepository;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -14,12 +16,21 @@ class EventVoter extends Voter
     public const VIEW = 'view';
     public const CAN_DELETE = 'CAN_DELETE';
     public const CAN_EDIT = 'CAN_EDIT';
+    public const CAN_PARTICIPATE = 'CAN_PARTICIPATE';
 
+    public const CAN_CANCEL = 'CAN_CANCEL';
     public const CREATE_TRANSPORT = 'CREATE_TRANSPORT';
+
+
+    public function __construct(
+        private readonly ParticipationRepository $participationRepository
+    )
+    {
+    }
 
     protected function supports($attribute, $subject): bool
     {
-        return in_array($attribute, [self::VIEW, self::CAN_DELETE, self::CAN_EDIT, self::CREATE_TRANSPORT])
+        return in_array($attribute, [self::VIEW, self::CAN_DELETE, self::CAN_EDIT, self::CAN_PARTICIPATE, self::CAN_CANCEL, self::CREATE_TRANSPORT])
             && $subject instanceof Event;
     }
 
@@ -39,11 +50,23 @@ class EventVoter extends Voter
         /** @var Event $event */
         $event = $subject;
 
+        $participation = $this->findParticipation($event,$user);
+
         return match ($attribute) {
             self::VIEW => $user->isParticipating($event),
             self::CREATE_TRANSPORT => $user->allowCreateTransport($event),
             self::CAN_DELETE, self::CAN_EDIT => $event->getUser() === $user,
+            self::CAN_PARTICIPATE => !$participation,
+            self::CAN_CANCEL => !empty($participation),
             default => false,
         };
+    }
+
+    private function findParticipation(Event $event, User $user): ?Participation
+    {
+        return $this->participationRepository->findOneBy([
+            'user' => $user->getId(),
+            'event' => $event->getId(),
+        ]);
     }
 }
---
+++ sources/src/Service/Mail/Sender.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Service\Mail;
+
+use App\Entity\Event;
+use App\Entity\User;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+
+class Sender
+{
+    public function __construct(
+        protected MailerInterface $mailer,
+    ){}
+
+    /**
+     * @throws TransportExceptionInterface
+     */
+    public function sendEventParticipation(Event $event, User $user): void
+    {
+        try {
+            $email = new TemplatedEmail();
+            $email->from(new Address('admin@gmail.com', 'Admin'))
+                ->subject("Participation à l'évênement : ".$event->getTitle())
+                ->to($user->getEmail())
+                ->htmlTemplate('emails/participation_event.html.twig')
+                ->context([
+                    'user' => $user,
+                    'event' => $event,
+                ]);
+            $this->mailer->send($email);
+        } catch (\Exception $e) {
+
+        }
+
+
+    }
+}
