9ecea092341ee1e190fe687a8f72d868e95b38db
MrAuGIR <augirard17@gmail.com>
2025-03-29 00:02:19 +0100

    wip: sendemailmessagehandler

--- sources/src/Message/Handler/SendEmailMessageHandler.php
+++ sources/src/Message/Handler/SendEmailMessageHandler.php
@@ -3,10 +3,22 @@
 namespace App\Message\Handler;
 
 use App\Message\SendEmailMessage;
+use App\Service\Mail\Sender;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
 class SendEmailMessageHandler
 {
-  public function __invoke(SendEmailMessage $message): void {}
+    public function __construct(
+        private Sender $sender
+    ) {}
+
+    public function __invoke(SendEmailMessage $message): void
+    {
+        $this->sender->send(
+            $message->getObject(),
+            $message->getMatch(),
+            $message->getUser()
+        );
+    }
 }
