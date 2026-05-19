ea588ca7e8f764deb00c6636b3fa95cdc05c32d0
MrAuGIR <augirard17@gmail.com>
2026-05-19 22:03:15 +0200

    [REFACTO] Extraction d'une couche service et correction des requêtes Doctrine.

--- sources/public/script/images/orderImage.js
+++ sources/public/script/images/orderImage.js
@@ -1,9 +1,11 @@
 //gestion de l'importance des images
 let inputs = document.querySelectorAll("[data-order]")
 for (input of inputs) {
-    input.addEventListener("change", function(e){    
+    input.addEventListener("change", function(e){
         let xmlhttp = new XMLHttpRequest;
-        xmlhttp.open("get", `/event/picture/order/${e.target.value}/${this.dataset.id}`)
-        xmlhttp.send()
+        xmlhttp.open("post", `/event/picture/order/${this.dataset.id}`)
+        xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+        let params = `value=${encodeURIComponent(e.target.value)}&_token=${encodeURIComponent(this.dataset.token)}`
+        xmlhttp.send(params)
     })
 }
